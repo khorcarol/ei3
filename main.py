68d916f16baec26bb14a585604425e22721aa996
shunt <shunting.khor1@gmail.com>
2024-07-22 15:41:19 +0200

    before starting sql data implementation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import time
 
 
 # Example usage (assuming sensor and feature_extraction modules are implemented)
-sensor = Sensor()
+sensor = Sensor(ip_address="cogsihq.dyndns.org:8000")
 sampling_rate = 100
 data_dir = "data"  # Adjust data directory if needed
 
@@ -13,6 +13,6 @@ data_acquisition = DataAcquisition(sensor, sampling_rate, data_dir)
 data_acquisition.start_acquisition()
 
 
-# from visualisation import real_time_features,real_time_spectrogram
+from visualisation import real_time_features,real_time_spectrogram
 # real_time_features('data')
-# real_time_spectrogram('data')
+real_time_spectrogram('data')
